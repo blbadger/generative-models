fixed mask for backprop

mixer_lm/custom_trainer.py
@@ -23,8 +23,6 @@ from transformers import LlamaConfig, LlamaForCausalLM
 from rotary_embedding_torch import RotaryEmbedding
 
 
-rotary_emb = RotaryEmbedding(dim = 256).to(0)
-
 def FeedForward(dim, expansion_factor=4):
 	inner_dim = int(dim * expansion_factor)
 	return nn.Sequential(
@@ -36,32 +34,32 @@ def FeedForward(dim, expansion_factor=4):
 def ConvForward(dim, expansion_factor=1):
 	inner_dim = int(dim * expansion_factor)
 	return nn.Sequential(
-		nn.Conv1d(dim, inner_dim, 1),
+		nn.Conv1d(dim, inner_dim, 1, bias=False),
 		nn.GELU(),
-		nn.Conv1d(inner_dim, dim, 1)
+		nn.Conv1d(inner_dim, dim, 1, bias=False)
 		)
 
 class MixerBlock(nn.Module):
 
-	def __init__(self, dim, length, mixer_mask=True, expand_conv=False):
+	def __init__(self, dim, length, mixer_mask=True, expand_conv=True):
 		super().__init__()
 		self.patch_layernorm = nn.LayerNorm(dim)
 		self.seq_layernorm = nn.LayerNorm(dim)
 		self.dim = dim
 		self.length = length
 		self.patch_ff = FeedForward(dim)
-		self.expand_conv = expand_conv
-		if self.expand_conv:
+		if expand_conv:
 			self.conv = ConvForward(length)
 		else:
 			self.conv = nn.Conv1d(length, length, 1)
-		
-		# for CLM training, apply lower triangular mask to convolution weights
 		self.mixer_mask = mixer_mask
+		self.expand_conv = expand_conv
 
 	def forward(self, x: torch.tensor):
 		if x.dim() > 3:
 			x = rearrange(x, 'b p t f -> (b p) t f')
+
+		# for CLM training, apply lower triangular mask to convolution weights
 		if self.mixer_mask:
 			if self.expand_conv:
 				rearranged_shape = rearrange(self.conv[0].weight, 'f d p -> f (d p)').shape
@@ -73,12 +71,13 @@ class MixerBlock(nn.Module):
 				mask = torch.tril(torch.ones(rearranged_shape))
 				applied_mask = rearrange(self.conv[2].weight, 'f d p -> f (d p)') * mask
 				self.conv[2].weight.data = rearrange(applied_mask, 'f (d p) -> f d p', p=1)
+
 			else:
 				rearranged_shape = rearrange(self.conv.weight, 'f d p -> f (d p)').shape
-				mask = torch.tril(torch.ones(rearranged_shape)).to(device)
+				mask = torch.tril(torch.ones(rearranged_shape))
 				applied_mask = rearrange(self.conv.weight, 'f d p -> f (d p)') * mask
 				self.conv.weight.data = rearrange(applied_mask, 'f (d p) -> f d p', p=1)
-
+				
 		residual = x
 		x = self.seq_layernorm(x)
 		x = self.conv(x) + residual
@@ -118,73 +117,29 @@ class LanguageMixer(nn.Module):
 		loss = self.cel(shift_logits, shift_labels)
 		return loss, output
 
+
 # tokenizer = AutoTokenizer.from_pretrained("huggyllama/llama-7b")
 tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tiny_token_4k")
 tokenizer.pad_token = tokenizer.eos_token
-n_vocab = len(tokenizer)
-print (tokenizer.is_fast)
 
+n_vocab = len(tokenizer)
 tokenized_length = 512
 dim = 128
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
-model = LanguageMixer(n_vocab, dim, 8).float().to(device)
+model = LanguageMixer(n_vocab, dim, 1).float().to(device)
+print (model)
 
-# one = torch.tensor([[[1, 4, 3]]]).to(device)
+# one = torch.tensor([[[5, 2, 3]]]).to(device)
 # two = torch.tensor([[[1, 2, 3]]]).to(device)
 # print (model(one, labels=one))
 # print (model(two, labels=two))
 # print (model)
 
-def count_parameters(model):
-	table = PrettyTable(["Modules", "Parameters"])
-	total_params = 0
-	print ()
-	for name, parameter in model.named_parameters():
-		if not parameter.requires_grad:
-			continue
-		params = parameter.numel()
-		table.add_row([name, params])
-		total_params += params
-	print(table)
-	print(f"Total Trainable Params: {total_params}")
-	return total_params
-
-count_parameters(model)
-
 # cached dataset
 train_text = load_dataset("roneneldan/TinyStories", split="train")
 valid_text = load_dataset("roneneldan/TinyStories", split="validation")
 
-def tile_inputs(input_ids, tile_overlap=100, tile_size=828):
-	text_length = len(input_ids[0])
-	assert text_length > tile_overlap, 'Text must be longer than overlap to tile'
-	tiled_arr = []
-	i = 0
-	while i < text_length:
-		if i + tile_size <= text_length:
-			tiled_arr.append(input_ids[0][i:i+tile_size])
-		else:
-			# pad the last tile to the appropriate length
-			tokens = input_ids[0][i:i+tile_size]
-			pad_length = tile_size - len(tokens)
-			tokens = torch.nn.functional.pad(tokens,
-											(0, pad_length),
-											 mode='constant',
-											 value=tokenizer.pad_token_id)
-			tiled_arr.append(tokens)
-		i += tile_size - tile_overlap
-	return tiled_arr
-
-def debatch_input(input_data):
-	output = []
-	for i in range(len(input_data)):
-		if input_data[i].dim() > 1:
-			input_data[i] = input_data[i].unsqueeze(1)
-			output += list(input_data[i])
-	return output
-
-
-def batch_tokenize_input(train_text, test_text, length=20000, batch_size=1024):
+def batch_tokenize_input(train_text, test_text, length=20000, batch_size=32):
 	train_data, test_data = [], []
 	max_length = 512
 
@@ -210,57 +165,10 @@ def batch_tokenize_input(train_text, test_text, length=20000, batch_size=1024):
 		).input_ids
 		test_data.append(input_ids)
 
-	train_data = debatch_input(train_data)
-	test_data = debatch_input(test_data)
-
-	return train_data, test_data
-
-def tokenize_input(train_text, test_text):
-	train_data, test_data = [], []
-	max_length = 512
-
-	for i in range(1000000):
-		input_ids = tokenizer.encode(
-			train_text[i]['text'],
-			add_special_tokens=False,
-			return_tensors='pt',
-			truncation=False,
-			max_length=max_length,
-			padding='max_length'
-		)
-
-		if len(input_ids[0]) > max_length:
-			input_set = tile_inputs(input_ids, tile_size=max_length)
-			for inp in input_set:
-				train_data.append(inp)
-		else:
-			train_data.append(input_ids)
-
-	for i in range(len(test_text)):
-		if test_text[i]:
-			input_ids = tokenizer.encode(
-				test_text[i]['text'],
-				add_special_tokens=False,
-				return_tensors='pt',
-				truncation=False,
-				max_length=max_length,
-				padding='max_length'
-			)
-
-			if len(input_ids[0]) > max_length:
-				input_set = tile_inputs(
-					input_ids,
-					tile_size=max_length
-				)
-				for inp in input_set:
-					test_data.append(inp)
-			else:
-				test_data.append(input_ids)
 
 	return train_data, test_data
 
 train_data, test_data = batch_tokenize_input(train_text, valid_text)
-train_data, test_data = debatch_input(train_data), debatch_input(test_data)
 
 def reformat_inputs(train_data, test_data):
 	# reformat inputs for transformer modelz`
@@ -272,57 +180,27 @@ def reformat_inputs(train_data, test_data):
 	return train_data, test_data
 
 
-if isinstance(model, LlamaForCausalLM):
-	reformat_inputs(train_data, test_data)
+optimizer = torch.optim.AdamW(model.parameters(), lr=1e-4)
+
+def train_model():
+	model.train()
+	epochs = 2
+	for epoch in range(epochs):
+		total_loss = 0
+		for step, batch in enumerate(train_data):
+			batch = rearrange(batch, 'b (p t) -> b p t', p=1)
+			optimizer.zero_grad()
+			batch = batch.to(device) # discard class labels
+			loss, output = model(batch, batch)
+			total_loss += loss.item()
+			loss.backward()
+			print (model.mixerblocks[0].conv.weight.grad[10][:10])
+			optimizer.step()
+			print (model.mixerblocks[0].conv.weight[10][:10])
+		print ('Average loss: ', total_loss / len(batch))
 
 
-mlflow.end_run()
-# training_arguments = transformers.TrainingArguments(
-# 	num_train_epochs=1,
-# 	per_device_train_batch_size=16,
-# 	per_device_eval_batch_size=32,
-# 	warmup_steps=500,
-# 	eval_steps=1000,
-# 	save_steps=1000,
-# 	learning_rate=1e-4,
-# 	fp16=True, 
-# 	evaluation_strategy='steps',
-# 	output_dir='~/Desktop/tinystories_mixer_full',
-# 	optim='adamw_torch',
-# 	overwrite_output_dir=True,
-# ) 
-
-print ('training begun')
-
-training_arguments = transformers.TrainingArguments(
-	num_train_epochs=20,
-	per_device_train_batch_size=16,
-	per_device_eval_batch_size=16,
-	warmup_steps=500,
-	eval_steps=2000,
-	save_steps=2000,
-	learning_rate=2e-4,
-	fp16=True, 
-	evaluation_strategy='steps',
-	output_dir='~/Desktop/tinystories_mixer_overfit',
-	optim='adamw_torch',
-	overwrite_output_dir=True,
-	save_safetensors=False
-)
-
-trainer = transformers.Trainer(
-	model=model,
-	train_dataset=train_data,
-	eval_dataset=test_data,
-	args=training_arguments,
-	data_collator=transformers.DataCollatorForLanguageModeling(tokenizer, mlm=False),
-)
-
-model.train()
-trainer.train()
-
-for name, param in model.named_parameters():
-	print (name)
+train_model()
 
 
 