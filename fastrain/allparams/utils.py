91aef1de331c3b6a03984e6cb6f01934ef60ee8c
blbadger <blbadger10@gmail.com>
2024-10-02 14:16:12 -0400

    utils rewrite

--- fastrain/allparams/utils.py
+++ fastrain/allparams/utils.py
@@ -3,7 +3,7 @@ from enum import Enum
 import torch
 from datasets import DatasetDict, load_dataset, load_from_disk
 from datasets.builder import DatasetGenerationError
-from transformers import AudoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
+from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 from peft import LoraConfig
 
 def create_and_prepare_model(args, data_args, training_args):
@@ -11,11 +11,14 @@ def create_and_prepare_model(args, data_args, training_args):
 	torch_dtype = quant_storage_dtype if quant_storage_dtype and quant_storage_dtype.is_floating_point else torch.float32
 	model = AutoModelForCausalLM.from_pretrained(
 			args.model_name_or_path,
+			quantization_config = bnb_config,
 			trust_remote_code=True,
 			attn_implementation="flash_attention_2" if args.use_flash_attn else "eager",
 			torch_dtype=torch_dtype
 		)
-
+	
+	# TODO: add special token compatibility
+	special_tokens=None
 	if special_tokens is not None:
 		tokenizer = AutoTokenizer.from_pretrained(
 			args.model_name_or_path,
