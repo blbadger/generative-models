89921ae85921e25c0a029d88d1e1a67cd937818b
Benjamin Badger <blbadger10@gmail.com>
2024-11-04 16:39:44 -0500

    block prediction and clean

---
+++ mixer_lm/fineweb_token_cleaner.py
@@ -0,0 +1,47 @@
+import os
+from datasets import load_dataset, load_from_disk, Dataset
+import argparse
+
+parser = argparse.ArgumentParser(description='Arg parser')
+parser.add_argument('--path', type=str)
+
+def clean(example):
+    for col in list(example.keys()):
+    	if col not in ['input_ids', 'attention_mask']:
+    		del example[col]
+    return example
+
+if __name__ == '__main__':
+	args = parser.parse_args()
+	path = args.path
+	dataset = load_from_disk(path)
+	print ('pre clean', dataset[0])
+	dataset = dataset.map(clean, batched=True)
+	print ('post clean', dataset[0])
+	# test_dataset.save_to_disk(test_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
