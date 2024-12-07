beb465c2b2a675b8a49e358bfb04c1917ac2bb4a
Benjamin Badger <blbadger10@gmail.com>
2024-12-06 21:02:06 -0500

    dataset reform

--- mixer_lm/tokenize_summaries.py
+++ mixer_lm/tokenize_summaries.py
@@ -21,22 +21,30 @@ def tokenization(example, n_ctx=128):
 		).input_ids
     return {'input_ids': tokens}
 
-def map_dataset(path):
+def map_dataset(array, path):
 	"""
 	Map dataset to tokens. Suitable for large datasets, note that split_index is low (5k means hold out 5k rows from training)
 	"""
-	text = load_dataset("HuggingFaceFW/fineweb-edu", split="train", name="sample-10BT", streaming=False)
-	dataset = text.map(tokenization, batched=False)
-	dataset.save_to_disk(path)
-	print ('Dataset saved to disk')
-	return
+	tokenized_array = []
+	for sample in array:
+		print (sample)
+		tokens = tokenizer.encode_plus(
+			sample,
+			add_special_tokens=False,
+			return_tensors='pt',
+			truncation=True,
+			padding=True,
+			padding_side='left'
+		).input_ids
+	output_dict = {'input_ids': tokenized_array}
+	return datasets.from_dict(tokenized_array)
 
 query_text = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_0_50000.json'))]
 query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_50000_100000.json'))]
 query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_100000_150000.json'))]
 query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_150000_200000.json'))]
 
-path = "/home/bbadger/Desktop/contrastive-summaries-fineweb-rpad-200k"
+path = "/home/bbadger/Desktop/contrastive-summaries-fineweb-lpad-200k"
 map_dataset(path)
 
 
