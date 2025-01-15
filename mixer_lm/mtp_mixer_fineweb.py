e5faf05f79bf055720df10abed88d0b3e87f84d0
Benjamin Badger <blbadger10@gmail.com>
2025-01-15 18:46:03 -0500

    mtp trainer

--- mixer_lm/mtp_mixer_fineweb.py
+++ mixer_lm/mtp_mixer_fineweb.py
@@ -32,66 +32,6 @@ def ConvForward(dim, expansion_factor=1):
 		nn.Conv1d(inner_dim, dim, 1)
 		)
 
-class DoubleMixerBlock(nn.Module):
-
-	def __init__(self, dim, length, clm_mask=False, expand_conv=False):
-		super().__init__()
-		self.patch_layernorm = nn.LayerNorm(dim)
-		self.seq_layernormf = nn.LayerNorm(dim)
-		self.seq_layernormr = nn.LayerNorm(dim)
-		self.dim = dim
-		self.length = length
-		self.patch_ff = FeedForward(dim)
-		if expand_conv:
-			self.conv = ConvForward(length)
-		else:
-			self.convf = nn.Conv1d(length, length, 1)
-			self.convr = nn.Conv1d(length, length, 1)
-		self.clm_mask = clm_mask
-		self.expand_conv = expand_conv
-		self.softmax = nn.Softmax(dim=0)
-
-	def forward(self, x: torch.tensor, y: torch.tensor):
-		if x.dim() > 3:
-			x = rearrange(x, 'b p t f -> (b p) t f')
-			y = rearrange(y, 'b p t f -> (b p) t f')
-
-		# for CLM training, apply lower triangular mask to convolution weights
-		if self.clm_mask:
-			if self.expand_conv:
-				rearranged_shape = rearrange(self.conv[0].weight, 'f d p -> f (d p)').shape
-				mask = torch.tril(torch.ones(rearranged_shape)).to(device)
-				applied_mask = rearrange(self.conv[0].weight, 'f d p -> f (d p)') * mask
-				self.conv[0].weight.data = rearrange(applied_mask, 'f (d p) -> f d p', p=1)
-
-				rearranged_shape = rearrange(self.conv[2].weight, 'f d p -> f (d p)').shape
-				mask = torch.tril(torch.ones(rearranged_shape)).to(device)
-				applied_mask = rearrange(self.conv[2].weight, 'f d p -> f (d p)') * mask
-				self.conv[2].weight.data = rearrange(applied_mask, 'f (d p) -> f d p', p=1)
-
-			else:
-				rearranged_shape = rearrange(self.conv.weight, 'f d p -> f (d p)').shape
-				# # softmax weights
-				# self.conv.weight.data = self.softmax(self.conv.weight.data)
-				mask = torch.tril(torch.ones(rearranged_shape)).to(device)
-				applied_mask = rearrange(self.conv.weight, 'f d p -> f (d p)') * mask
-				self.conv.weight.data = rearrange(applied_mask, 'f (d p) -> f d p', p=1)
-
-		else:
-
-			masked_convf = torch.tril(rearrange(self.convf.weight, 'f d p -> p f d'))
-			self.convf.weight.data = rearrange(masked_convf, 'p f d -> f d p').contiguous()
-
-			masked_convr = torch.triu(rearrange(self.convr.weight, 'f d p -> p f d'), diagonal=2)
-			self.convr.weight.data = rearrange(masked_convr, 'p f d -> f d p').contiguous()
-
-		residualf, residualr = x, y
-		x, y = self.seq_layernormf(x), self.seq_layernormr(y)
-		x, y = self.convf(x) + residualf, self.convr(y) + residualr
-		residualf, residualr = x, y
-		x, y = self.patch_layernorm(x), self.patch_layernorm(y)
-		x, y = self.patch_ff(x) + residualf, self.patch_ff(y) + residualr
-		return x, y
 
 class MixerBlock(nn.Module):
 
@@ -177,38 +117,9 @@ class MixerHead(nn.Module):
 		hidden_layer = self.out_proj(hidden_layer)
 		return hidden_layer
 
-class MLPMixerBlock(nn.Module):
-
-	def __init__(self, dim, length, **kwargs):
-		super().__init__()
-		self.patch_layernorm = nn.LayerNorm(dim)
-		self.seq_layernorm = nn.LayerNorm(dim)
-		self.dim = dim
-		self.length = length
-		self.patch_ff = FeedForward(dim)
-		self.conv = nn.Linear(length, length)
-
-
-	def forward(self, x: torch.tensor):
-		if x.dim() > 3:
-			x = rearrange(x, 'b p t f -> (b p) t f')
-
-		self.conv.weight.data = torch.tril(self.conv.weight)
-		residual = x
-		x = self.seq_layernorm(x)
-		x = rearrange(x, 'b t h -> b h t')
-		x = self.conv(x)
-		x = rearrange(x, 'b h t -> b t h')
-		x += residual
-
-		residual = x
-		x = self.patch_layernorm(x)
-		x = self.patch_ff(x) + residual
-		return x
-
 class LanguageMixer(nn.Module):
 
-	def __init__(self, n_vocab, dim, depth, tie_weights=False):
+	def __init__(self, n_vocab, dim, depth, tie_weights=False, n_passes=2):
 		super().__init__()
 		self.wte = nn.Embedding(n_vocab, dim)
 		self.mixerblocks = nn.ModuleList(
@@ -219,11 +130,11 @@ class LanguageMixer(nn.Module):
 				)
 			for i in range(depth)]
 			).to(device)
-		#self.up_proj1, self.up_proj2 = nn.Linear(dim, dim*2), nn.Linear(dim*2, dim*3)
 		self.lm_head = nn.Linear(dim, n_vocab, bias=False)
 		if tie_weights:
 			 self.wte.weight = self.lm_head.weight
 		self.cel = nn.CrossEntropyLoss()
+		self.n_passes = n_passes
 
 	def forward(self, input_ids, labels=None, **kwargs):
 		x = input_ids
@@ -238,6 +149,24 @@ class LanguageMixer(nn.Module):
 		shift_logits = output[..., :-1].contiguous()
 		shift_labels = labels[..., 1:].contiguous()
 		loss = self.cel(shift_logits, shift_labels)
+
+		for i in range(self.n_passes-1):
+			# assign tokens
+			x = torch.argmax(output, dim=-2)
+			# shift labels
+			labels = labels[..., 1:].contiguous()
+
+			x = self.wte(x)
+			for i, block in enumerate(self.mixerblocks):
+				x = block(x)
+			output = self.lm_head(x)
+			if labels.dim() > 2:
+				labels = rearrange(labels, 'b p t -> b (p t)')
+			output = rearrange(output, 'b t e -> b e t')
+			shift_logits = output[..., :-(1 + i)].contiguous()
+			shift_labels = labels[..., 1:].contiguous()
+			loss += self.cel(shift_logits, shift_labels)
+
 		return loss, output
 
 
