3dbb293e0e0cc0fede813b8cb014232f1918c0cb
Benjamin Badger <blbadger10@gmail.com>
2025-01-21 12:28:55 -0500

    debugged mtp mixer

--- mixer_lm/mtp_mixer_fineweb.py
+++ mixer_lm/mtp_mixer_fineweb.py
@@ -185,14 +185,14 @@ tokenizer.pad_token = tokenizer.eos_token
 n_vocab = len(tokenizer)
 print ('Vocab size: ', n_vocab)
 
-tokenized_length = 1024
+tokenized_length = 32
 dim = 1024
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 #model = MultiHeadedMixer(n_vocab, dim, 8, heads=4).float().to(device)
 model = LanguageMixer(n_vocab, dim, 16).float()
 count_parameters(model)
-train_path = "/home/bbadger/Desktop/finemath-4-tokenized-train-c1024-8k"
-test_path = "/home/bbadger/Desktop/finemath-4-tokenized-test-c1024-8k"
+train_path = "/home/bbadger/Desktop/finemath-4-tokenized-train-c32-8k-debatched"
+test_path = "/home/bbadger/Desktop/finemath-4-tokenized-test-c32-8k-debatched"
 
 def tokenization(example):
 	tokens = tokenizer.batch_encode_plus(
@@ -230,15 +230,15 @@ print ('training begun')
 print (train_dataset[0])
 training_arguments = transformers.TrainingArguments(
 	num_train_epochs=2,
-	per_device_train_batch_size=8,
-	per_device_eval_batch_size=8,
+	per_device_train_batch_size=256,
+	per_device_eval_batch_size=256,
 	warmup_steps=500,
 	eval_steps=4000,
 	save_steps=8000,
 	learning_rate=5e-4,
 	fp16=True,
 	evaluation_strategy='steps',
-	output_dir='~/Desktop/mtp_finemath_mixer_1024_n16',
+	output_dir='~/Desktop/mtp_finemath_mixer_1024_n16_c32',
 	optim='adamw_torch',
 	overwrite_output_dir=True,
 	save_safetensors=True,
