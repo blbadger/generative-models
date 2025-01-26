7cb0b60debeb364b2cddcb04230553aa78121796
blbadger <blbadger10@gmail.com>
2025-01-26 02:55:07 +0000

    debugged e5

--- mixer_lm/e5_mistral_benchmark.py
+++ mixer_lm/e5_mistral_benchmark.py
@@ -1,25 +1,57 @@
 import torch
 import torch.nn.functional as F
-
+import datasets
 from torch import Tensor
-from transformers import AutoTokenizer, AutoModel
+from transformers import AutoTokenizer, AutoModel, BitsAndBytesConfig
+import json
+import random
+from accelerate import infer_auto_device_map
+from safetensors.torch import load_model
+from transformers import LlamaModel, LlamaConfig, LlamaForCausalLM
 
-
-tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct")
-model = AutoModel.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct")
+bnb_config = BitsAndBytesConfig(
+load_in_4bit=True,
+bnb_4bit_use_double_quant=True,
+bnb_4bit_quant_type="nf4",
+bnb_4bit_compute_dtype=torch.float16
+)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+#tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct")
+#model = AutoModel.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct", device_map='auto')
 reverse_tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tokenizer_fineweb_8k")
 
-def generate_sample(self, query_dataset, target_dataset, index, dataset_size=20000, n_context=512, replace=False):
-	self.n_context = n_context
-	prob_weights = torch.ones(target_embeddings.shape[0])
-	input = torch.zeros((n_context, query_embeddings[0].shape[1]))
-	input[0] = query_dataset[index]
-	prob_weights[index] = 0
+tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tokenizer_fineweb_8k")
+tokenizer.pad_token = '<|end_of_text|>'
+dim = 512
+context_length = 512
+llama_config_kwargs = {
+    'hidden_size': dim,
+    'intermediate_size': 4*dim,
+    'num_hidden_layers': 16,
+    'num_attention_heads': 4,
+    'vocab_size': 8000
+}
+
+# Initializing a LLaMA model
+configuration = LlamaConfig(**llama_config_kwargs)
+
+# Initializing a model from the llama-7b style configuration
+model = LlamaForCausalLM(configuration).float().to(device)
+load_model(model, "/home/bbadger/Desktop/fineweb_llama_n16_h4_b32/checkpoint-200000/model.safetensors")
+def generate_sample(query_dataset, target_dataset, index, dataset_size=20000, start_index=180000, n_context=128, replace=False):
+	prob_weights = torch.ones(dataset_size)
+	input = [query_dataset[index]]
+	#print (reverse_tokenizer.decode(target_dataset[index]['input_ids']))
+	#print (input)
+	prob_weights[index-start_index] = 0
 	indices = torch.multinomial(prob_weights, n_context-1, replacement=replace)
-	input[1:] = target_dataset[indices]
-	target_index = random.randint(1, self.n_context-1) # random index to put target embedding
-	matching_target = self.target_embeddings[index] # target the query matches
-	input[target_index] = target_dataset[index]
+	for i in indices:
+		target_text = reverse_tokenizer.decode(target_dataset[int(i)]['input_ids'])
+		#print (target_text)
+		input.append(str(target_text))
+	target_index = random.randint(1, n_context-1) # random index to put target embedding
+	#print (target_index, index, len(input))
+	input[target_index] = reverse_tokenizer.decode(target_dataset[int(index)]['input_ids'])
 	return input, target_index
 
 
@@ -38,44 +70,44 @@ def get_detailed_instruct(task_description: str, query: str) -> str:
 	return f'Instruct: {task_description}\nQuery: {query}'
 
 
+
 total_correct = 0
 total = 0
+# Each query must come with a one-sentence instruction that describes the task
+target_dataset = datasets.load_from_disk('/home/bbadger/Desktop/fineweb-edu-tokenized-train-c512')
+query_dataset = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_0_50000.json'))]
+query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_50000_100000.json'))]
+query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_100000_150000.json'))]
+query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_150000_200000.json'))]
+	
 for i in range(180000, 200000):
 	# Each query must come with a one-sentence instruction that describes the task
-	target_dataset = datasets.load_from_disk('fineweb-edu-tokenized-train-c512')
-	query_dataset = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_0_50000.json'))]
-	query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_50000_100000.json'))]
-	query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_100000_150000.json'))]
-	query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_150000_200000.json'))]
-	n_sampes = 128
+	n_samples = 32
 	task = 'Given a summary of a passage, find the corresponding text.'
-	query = queries[i]
 	queries = [
-		get_detailed_instruct(task, query),
+		get_detailed_instruct(task, query_dataset[i]),
 	]
 	# No need to add instruction for retrieval documents
-	samples, target_index = generate_samples(query_dataset, target_dataset, i)
-	for i in range(len(samples)):
-		samples[i] = reverse_tokenizer.decode(samples[i])
+	samples, target_index = generate_sample(query_dataset, target_dataset, i, n_context=n_samples)
 
-	input_texts = queries + samples
-	tokenizer = AutoTokenizer.from_pretrained('intfloat/e5-mistral-7b-instruct')
-	model = AutoModel.from_pretrained('intfloat/e5-mistral-7b-instruct')
-
+	#samples[0] = str(queries[0])
+	samples[0] = query_dataset[i]
 	max_length = 512
 	# Tokenize the input texts
-	batch_dict = tokenizer(input_texts, max_length=max_length, padding=True, truncation=True, return_tensors='pt')
+	batch_dict = tokenizer.encode(samples, max_length=max_length, padding=True, truncation=True, return_tensors='pt').to(device)
+	with torch.no_grad():
+		outputs = model(**batch_dict)
+		print('output given')
+		embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
 
-	outputs = model(**batch_dict)
-	embeddings = last_token_pool(outputs.last_hidden_state, batch_dict['attention_mask'])
-
-	# normalize embeddings
-	embeddings = F.normalize(embeddings, p=2, dim=1)
-	scores = (embeddings[:1] @ embeddings[1:].T) * 100
-	top_index = torch.topk(scores, 1)
-	if top_index == target_index:
-		total_correct += 1
-	total += 1
-	print (f'Top-1 accuracy: ', total_correct / total)
+		# normalize embeddings
+		embeddings = F.normalize(embeddings, p=2, dim=1)
+		scores = (embeddings[:1] @ embeddings[1:].T) * 100
+		top_index = int(torch.topk(scores, 1).indices[0])
+		print ('Top index, target index', top_index, target_index)
+		if top_index+1 == target_index:
+			total_correct += 1
+		total += 1
+		print (f'Top-1 accuracy: ', total_correct / total)
 
 	
