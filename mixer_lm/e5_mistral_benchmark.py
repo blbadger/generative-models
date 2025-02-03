e3c14f6e9979b5aea80addbcb1456e74e8b0e150
Benjamin Badger <blbadger10@gmail.com>
2025-02-03 11:25:36 -0500

    e5 mistral finemath benchmark

--- mixer_lm/e5_mistral_benchmark.py
+++ mixer_lm/e5_mistral_benchmark.py
@@ -9,18 +9,6 @@ from accelerate import infer_auto_device_map
 from safetensors.torch import load_model
 from transformers import LlamaModel, LlamaConfig, LlamaForCausalLM
 
-bnb_config = BitsAndBytesConfig(
-	load_in_4bit=True,
-	bnb_4bit_use_double_quant=True,
-	bnb_4bit_quant_type="nf4",
-	bnb_4bit_compute_dtype=torch.float16
-)
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct")
-model = AutoModel.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct", quantization_config=bnb_config, device_map='auto')
-reverse_tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tokenizer_fineweb_8k")
-
 def generate_sample(query_dataset, target_dataset, index, dataset_size=20000, start_index=180000, n_context=128, replace=False):
 	prob_weights = torch.ones(dataset_size)
 	input = [query_dataset[index]]
@@ -49,18 +37,41 @@ def get_detailed_instruct(task_description: str, query: str) -> str:
 	return f'Instruct: {task_description}\nQuery: {query}'
 
 
+bnb_config = BitsAndBytesConfig(
+	load_in_4bit=True,
+	bnb_4bit_use_double_quant=True,
+	bnb_4bit_quant_type="nf4",
+	bnb_4bit_compute_dtype=torch.float16
+)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct")
+model = AutoModel.from_pretrained("/home/bbadger/Desktop/e5-mistral-7b-instruct", quantization_config=bnb_config, device_map='auto')
+reverse_tokenizer = AutoTokenizer.from_pretrained("/home/bbadger/Desktop/tokenizer_fineweb_8k")
+
+def load_dataset(finemath=True):
+	if not finemath:
+		target_dataset = datasets.load_from_disk('/home/bbadger/Desktop/fineweb-edu-tokenized-train-c512')
+		query_dataset = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_0_50000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_50000_100000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_100000_150000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_150000_200000.json'))]
+
+	else:
+		target_dataset = datasets.load_from_disk('/home/bbadger/Desktop/finemath-4-tokenized-train-c512-lpad-8k')
+		query_dataset = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_0_50000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_50000_100000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_100000_150000.json'))]
+		query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_150000_200000.json'))]
+	return query_dataset
+
+
+query_dataset = load_dataset()
 total_correct = 0
 total = 0
-# Each query must come with a one-sentence instruction that describes the task
-target_dataset = datasets.load_from_disk('/home/bbadger/Desktop/fineweb-edu-tokenized-train-c512')
-query_dataset = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_0_50000.json'))]
-query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_50000_100000.json'))]
-query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_100000_150000.json'))]
-query_dataset += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/fineweb_retrieval_150000_200000.json'))]
-	
 for i in range(180000, 200000):
 	# Each query must come with a one-sentence instruction that describes the task
-	n_samples = 128
+	n_samples = 32
 	task = 'Given a summary of a passage, find the corresponding text.'
 	queries = [
 		get_detailed_instruct(task, query_dataset[i])
