a8ce36a4972ff859480a2364e7930a851a491730
Benjamin Badger <blbadger10@gmail.com>
2025-03-01 23:10:53 -0500

    debugged 800k retr

--- mixer_lm/tokenize_summaries.py
+++ mixer_lm/tokenize_summaries.py
@@ -54,7 +54,7 @@ def extract_tokens(dataset, limit=800000, label='text'):
 	output_dict = {label: torch.tensor(array)}
 	return output_dict
 
-load_finemath = False
+load_finemath = True
 if load_finemath:
 	query_text = [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_0_50000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_50000_100000.json'))]
@@ -63,6 +63,7 @@ if load_finemath:
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_200000_250000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_250000_300000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_300000_350000.json'))]
+	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_350000_400000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_400000_500000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_500000_600000.json'))]
 	query_text += [i['choices'][0]['message']['content'] for i in json.load(open('/home/bbadger/Desktop/finemath_retrieval_600000_700000.json'))]
@@ -87,8 +88,9 @@ else:
 
 summary_dataset = map_dataset(query_text, label='summary')
 text_dataset = load_from_disk(text_path, keep_in_memory=None)
+
 # assign limit to be the size of the summary dataset
-text_dataset = extract_tokens(text_dataset, limit=400000, label='text')
+text_dataset = extract_tokens(text_dataset, limit=800000, label='text')
 dataset = {**text_dataset, **summary_dataset}
 save_file(dataset, path)
 
